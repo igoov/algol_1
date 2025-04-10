52df6d430238830e0e6190395e65579db46b1964
MDchefin_ <123131848+igoov@users.noreply.github.com>
2025-04-10 15:19:03 -0300

    .

---
+++ 2 - Prova/Atividade 1/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    int antecessor = num - 1;
+    int sucessor = num + 1; 
+
+    printf("Antecessor: %d\n",antecessor);
+    printf("Sucessor: %d\n", sucessor);
+    return 0;
+}
